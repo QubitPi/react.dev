9eb5df48072bbff7b718bdbfd77232730dbd56ca
Jack <jack20191124@proton.me>
2023-03-21 15:01:57 +0800

    Publish forked site

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
+basePath: '/react.dev',
   pageExtensions: ['jsx', 'js', 'ts', 'tsx', 'mdx', 'md'],
   reactStrictMode: true,
   experimental: {
